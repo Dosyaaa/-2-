0c78af217fb6d01ae99a4233c60b386d7d55c872
Dosyaaa <116172781+Dosyaaa@users.noreply.github.com>
2022-10-21 17:51:06 +0600

    Update main.swift

--- Домашка 2ая/main.swift
+++ Домашка 2ая/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
-
+func square(square1: Int){
+    print("Нахождение квадрата числа:\(square1) * \(square1) = \(square1 * square1)")
+}
+square(square1: Int.random(in: 1...10))
+func cube(cube1: Int){
+    print("Нахождение куба числа:\(cube1) * \(cube1) * \(cube1) = \(cube1 * cube1 * cube1)")
+}
+cube(cube1: Int.random(in: 1...6))
+func pcube(pcube1: Int){
+    print("Нахождение периметра куба:\(pcube1) * \(8) = \(pcube1 * 8)")
+}
+pcube(pcube1: Int.random(in: 4...20))
+func S(S1: Int,S2: Int){
+    print("Нахождение площади прямоугольника:\(S1 + S2) * \(2) = \((S1 + S2) * 2)")
+}
+S(S1: Int.random(in: 1...10), S2: Int.random(in: 10...20))
+let nu: Double = 3.14
+func Skruga(nu: Double, r: Double){
+    print("Нахождение площади круга:\(2) * \(nu) * \(r * r) = \(2 * nu * (r * r)) ")
+}
+Skruga(nu: nu, r: Double.random(in: 1.0...11.48))
